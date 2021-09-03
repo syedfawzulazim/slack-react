cfd8d46064fe3306746a2e1e11962c64e5c4c89e
Syed Fawzul Azim <adnansyed27@gmail.com>
2021-09-03 04:38:14 +0200

    component slidebar done

--- src/components/slidebar/index.js
+++ src/components/slidebar/index.js
@@ -1,17 +1,51 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import './styles/slidebar.css'
-import { FiberManualRecord } from '@material-ui/icons'
+import SlidebarOption from './slidebarOptions/SlidebarOption'
+import { FiberManualRecord, CreateOutlined, InsertCommentOutlined, AddCircleOutlineOutlined, ExpandMoreOutlined, ExpandLessOutlined, FileCopyOutlined, AppsOutlined, PeopleAltOutlined, BookmarkBorderOutlined, InboxOutlined, DraftsOutlined } from '@material-ui/icons'
+import db from '../firebase'
+export default function Slidebar() {
 
-export default function index() {
+    const [channels, setChannels] = useState([])
+
+    useEffect(() => {
+        db.collection('rooms').onSnapshot(snapshot => {
+            setChannels(snapshot.docs.map(doc => ({
+                id: doc.id,
+                name: doc.data().name,
+            })))
+        })
+    }, [])
+
     return (
         <div className="slidebar">
             <div className="slidebar__header">
-                <h2>Syed Fawzul Azim</h2>
-                <h3>
-                    <FiberManualRecord />
-                    Adnan
-                </h3>
+                <div className="slidebar__info">
+
+                    <h2>Slack Project</h2>
+                    <h3>
+                        <FiberManualRecord />
+                        Syed Fawzul Azim
+                    </h3>
+                </div>
+                <CreateOutlined />
             </div>
+            <SlidebarOption Icon={InsertCommentOutlined} title="Threads" />
+            <SlidebarOption Icon={InboxOutlined} title="Mention & reaction" />
+            <SlidebarOption Icon={DraftsOutlined} title="Saved items" />
+            <SlidebarOption Icon={BookmarkBorderOutlined} title="Channel browser" />
+            <SlidebarOption Icon={PeopleAltOutlined} title="People & user groups" />
+            <SlidebarOption Icon={AppsOutlined} title="Apps" />
+            <SlidebarOption Icon={FileCopyOutlined} title="File browser" />
+            <SlidebarOption Icon={ExpandLessOutlined} title="Show less" />
+            <hr />
+            <SlidebarOption Icon={ExpandMoreOutlined} title="Channels" />
+            <SlidebarOption Icon={AddCircleOutlineOutlined} title="Add Channel" />
+
+            {
+                channels.map((channel) => (
+                    <SlidebarOption title={channel.name} id={channel.id} />
+                ))
+            }
         </div>
     )
 }
