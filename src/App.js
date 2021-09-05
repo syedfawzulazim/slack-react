4aec94cfd2411f6c2834ec1fb4b81553212e49ce
Syed Fawzul Azim <adnansyed27@gmail.com>
2021-09-05 04:30:29 +0200

    main files edited

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
 import "./Global.css"
-import { Header, Slidebar, Chat } from "./components";
+import React from "react";
+import { Header, Slidebar, Chat, Login } from "./components";
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import { useStateValue } from './stateProvider/StateProvider';
 
 function App() {
+  const [{ user }, dispatch] = useStateValue()
+
+
   return (
     <div className="app">
       <Router>
-        <Header />
-        <div className="app__body">
-          <Slidebar />
-          <Switch>
-            <Route path="/room/:roomId">
-              <Chat />
-            </Route>
-            <Route path="/">
-              <h1>Welcome</h1>
-            </Route>
-          </Switch>
-        </div>
+        {!user ? (
+          <Login />
+        ) :
+          (
+            <>
+              <Header />
+              <div className="app__body">
+                <Slidebar />
+                <Switch>
+                  <Route path="/room/:roomId">
+                    <Chat />
+                  </Route>
+                  <Route path="/">
+                    <h1>Welcome</h1>
+                  </Route>
+                </Switch>
+              </div>
+            </>
+
+          )
+        }
       </Router>
     </div>
   );
